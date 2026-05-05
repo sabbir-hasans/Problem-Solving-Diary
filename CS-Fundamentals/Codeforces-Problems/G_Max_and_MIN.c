ae55af072271465ad9f5e2e417717d3bacabe561
sabbir-hasans <sabbir.cse3@gmail.com>
2026-05-05 19:33:40 +0600

    Codeforces G_Max_and_MIN problems solved

---
+++ CS-Fundamentals/Codeforces-Problems/G_Max_and_MIN.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <limits.h>
+
+void fun(int a[], int n)
+{
+    int min = INT_MAX;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] < min)
+        {
+            min = a[i];
+        }
+    }
+
+    int max = INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] > max)
+        {
+            max = a[i];
+        }
+    }
+    printf("%d %d",min,max);
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    fun(a, n);
+    return 0;
+}
