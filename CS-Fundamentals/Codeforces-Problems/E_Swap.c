cf44894e2fd0a35b46ea0b86230c77b47b640f8f
sabbir-hasans <sabbir.cse3@gmail.com>
2026-05-05 07:55:24 +0600

    Codeforces problem: E_Swap submit

---
+++ CS-Fundamentals/Codeforces-Problems/E_Swap.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int swap (int X, int Y)
+{
+    int temp = X;
+    X = Y;
+    Y = temp;
+    printf("%d %d", X, Y);
+}
+int main()
+{
+    int X, Y;
+    scanf("%d %d", &X, &Y);
+    swap(X, Y);
+    return 0;
+}
