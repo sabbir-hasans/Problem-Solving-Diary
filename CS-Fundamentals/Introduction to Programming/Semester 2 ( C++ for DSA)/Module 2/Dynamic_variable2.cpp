8ae9f43f28997399f9fa7667459f88ced9ccddb2
sabbir-hasans <sabbir.cse3@gmail.com>
2026-05-21 10:18:52 +0600

    Module 2 upload C++

---
+++ CS-Fundamentals/Introduction to Programming/Semester 2 ( C++ for DSA)/Module 2/Dynamic_variable2.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+// int *p;
+// void fun()
+// {
+//     int x = 10;
+//     p = &x;
+//     cout
+//         << "FUN ->" << *p << endl;
+//     return;
+// }
+// int main()
+// {
+//     fun();
+//     cout
+//         << "MAIN ->" << *p << endl;
+//     return 0;
+// }
+int *p;
+void fun()
+{
+    int* x = new int;
+    *x = 10;
+    p = x;
+    cout
+        << "FUN ->" << *p << endl;
+    return;
+}
+int main()
+{
+    fun();
+    cout
+        << "MAIN ->" << *p << endl;
+    return 0;
+}
